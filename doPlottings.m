cb26bde3adc8ef69cb88df6cea96f5f89fd871c6
francisco-eduqo <francisco@eduqo.com.br>
2020-09-18 22:42:44 -0300

    Removing redundances and formatting code

--- doPlottings.m
+++ doPlottings.m
@@ -17,7 +17,7 @@ colorMatrix(1:6, 1:3)=[1, 0, 0;
 colorMatrix(7:N+2, 1:3)=hot((N+2-6));
 
 
-%Plotting observables
+%% Plotting observables
 
 plot(t(1:timeMax),I5(1:timeMax))
 grid on
@@ -29,8 +29,6 @@ plot(t(1:timeMax),E(1:timeMax))
 grid on
 title('Energy vs. Time')
 xlabel('time axis'), ylabel ('Energy')
-
-
 
 figure
 plot3(CM1(1:timeMax), CM2(1:timeMax), CM3(1:timeMax))
@@ -45,42 +43,29 @@ title('delta energy as a function of time')
 xlabel('time'), ylabel('delta energy')
 
 
-
-%PLOTS OF TRAJECTORIES
+%% PLOTS OF TRAJECTORIES
 figure
 hold on
-    for k=1:N
-        plot3(y(:, 3*k-2), y(:, 3*k-1), y(:, 3*k), 'Color', colorMatrix(k,:))
-        plot3(y(1, 3*k-2), y(1, 3*k-1), y(1, 3*k), 'o' ,'Color', colorMatrix(k,:))
-        plot3(y(timeMax, 3*k-2), y(timeMax, 3*k-1), y(timeMax, 3*k), '*','Color', colorMatrix(k,:))
-    end
-
+for k=1:N
+    plot3(y(:, 3*k-2), y(:, 3*k-1), y(:, 3*k), 'Color', colorMatrix(k,:))
+    plot3(y(1, 3*k-2), y(1, 3*k-1), y(1, 3*k), 'o' ,'Color', colorMatrix(k,:))
+    plot3(y(timeMax, 3*k-2), y(timeMax, 3*k-1), y(timeMax, 3*k), '*','Color', colorMatrix(k,:))
+end
 grid on
 title('The trajectories of the bodies (o is start, * is end)')
-xlabel('x axis'), ylabel('y axis'), zlabel('z axis')
-
-
-
-figure
-CM = [CM1' CM2' CM3'];
-hold on
- for k=1:N
-  plot3(y(:, 3*k-2)-CM(:,1), y(:, 3*k-1)-CM(:,2), y(:, 3*k)-CM(:,3),'Color', colorMatrix(k,:))
-  plot3(y(1, 3*k-2)-CM(1,1), y(1, 3*k-1)-CM(1,2), y(1, 3*k)-CM(1,3), 'o' ,'Color', colorMatrix(k,:))
-  plot3(y(timeMax, 3*k-2)-CM(timeMax,1), y(timeMax, 3*k-1)-CM(timeMax,2), y(timeMax, 3*k)-CM(timeMax,3), '*' ,'Color', colorMatrix(k,:))
- end
-grid on
-title('The trajectories of the bodies in center of mass coordinates (o is start, * is end)')
-xlabel('x axis'), ylabel('y axis'), zlabel('z axis')
-
+xlabel('x axis'), ylabel('y axis'), zlabel('z axis');
+axis equal;
 
 %%
 figure
-colors = ['r', 'b', 'g', 'k', 'y'];
-for k = 1:N
-    plot3(y(:,3*k-2), y(:,3*k-1), y(:,3*k), colors(mod(k, 5)));
-    hold on;
+CM = [CM1' CM2' CM3'];
+hold on
+for k=1:N
+    plot3(y(:, 3*k-2)-CM(:,1), y(:, 3*k-1)-CM(:,2), y(:, 3*k)-CM(:,3),'Color', colorMatrix(k,:))
+    plot3(y(1, 3*k-2)-CM(1,1), y(1, 3*k-1)-CM(1,2), y(1, 3*k)-CM(1,3), 'o' ,'Color', colorMatrix(k,:))
+    plot3(y(timeMax, 3*k-2)-CM(timeMax,1), y(timeMax, 3*k-1)-CM(timeMax,2), y(timeMax, 3*k)-CM(timeMax,3), '*' ,'Color', colorMatrix(k,:))
 end
 grid on
+title('The trajectories of the bodies in center of mass coordinates (o is start, * is end)')
 xlabel('x axis'), ylabel('y axis'), zlabel('z axis');
-title('Trajectories 3D in inertial frame');
+axis equal;
