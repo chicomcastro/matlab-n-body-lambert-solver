3d4043b59cbfbfd0c1f2e0f0b41054879595f4ab
francisco-eduqo <francisco@eduqo.com.br>
2020-09-18 23:52:23 -0300

    Refactor to use real data

--- setUpInitialConditions.m
+++ setUpInitialConditions.m
@@ -18,9 +18,32 @@ br = zeros(1,N);
 
 %initial velocities in v alpha and beta.
 v0 = 0.6*ones(1,N);
+v0(1) = 0;
 av = 90*ones(1,N)*c;
 bv = zeros(1,N)*c;
 
+
+%% Earth-Sun system
+M_sun = 2e30;   % kg
+M_earth = 6e24; % kg
+um = M_sun + M_earth;
+mu = M_earth/um;
+Mass(1) = 1 - mu;
+Mass(2) = mu;
+
+UA = 1.496e11;  % m
+R_earth_sun = 1*UA;
+ud = UA;
+r0(1) = 0;
+r0(2) = 1;
+
+ut = (ud^3/G_metric/um)^(1/2);  % s
+
+V_earth_sun = sqrt(G_metric*M_sun/R_earth_sun);
+v0(1) = 0;
+v0(2) = V_earth_sun/ud*ut;
+
+%%
 Rec = 0;
 %convert positions to cartesean frame
 
--- setUpParameters.m
+++ setUpParameters.m
@@ -4,26 +4,15 @@
 %--------------------------------------------------------------------------
 
 %As the user you specify these.  The Program does the rest.
-%Remember that the the system of ODEs is a 6*N system of first order
+%Remember that the system of ODEs is a 6*N system of first order
 %scalar ODEs and expect matlab to bog down accordingly.
 
-
-%If G=1 then integration show interesting behavior for 'simulationTime'
-%between 2 and 10.  For such values the program run in a resonable amount
-%of time for 'N' between 2 and 12.  (Run times often one minute or much
-%less, however if the randomly chosen initial conditions pass the system
-%near a singularity then the program will run slower even in this range).
-
-%I've run the program with as many as 40 bodies on a home desk top and
-%obtained results in an hour.  I've had out of memory errore for more than
-%45 bodies (with simulation times over 10).
-
-N=3                  %number of bodies
-simulationTime=10     %How long to run the simulation
-G=1                  %gravitational constant
+N=2                   % number of bodies
+simulationTime=10     % How long to run the simulation
+G=1                   % gravitational constant
 
 %The value of the gravitation constant in the metric system is
 %
-%G_metric=6.67300 × 10-11 (m3 kg-1 s-2)
+G_metric=6.67300e-11; % (m3 kg-1 s-2)
 %
 %which is very small
