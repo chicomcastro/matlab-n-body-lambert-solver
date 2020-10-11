f4cb85cff9e0928c9c7a75ebe3b5783fb98dddc0
francisco-eduqo <francisco@eduqo.com.br>
2020-10-11 00:30:28 -0300

    Implementing stop of integrationn if it takes to long

--- runIntegration.m
+++ runIntegration.m
@@ -10,8 +10,20 @@ if exist('simulationTime', 'var')
 end
 
 if ~exist('options', 'var')
-    options=odeset('RelTol',1e-12,'AbsTol',1e-12);
+    tstart = tic;
+    options=odeset('RelTol',1e-12,'AbsTol',1e-12,'Events',@(t,y)myEvent(t,y,tstart));
 end
 
 %Integrate the System
-[t,y] = ode113('nBodyWpar',tspan,y0,options,flag,N,G,Mass);
+[t,y] = ode113(@(t,y)nBodyWpar(t,y,options,flag,N,G,Mass),tspan,y0,options);
+
+function [value, isterminal, direction] = myEvent(t, y, tstart)
+    
+    value      = toc(tstart) < 1.0;
+    isterminal = 1;   % Stop the integration
+    direction  = 0;
+    
+    if value == 0
+        disp("Exiting ode113");
+    end
+end
